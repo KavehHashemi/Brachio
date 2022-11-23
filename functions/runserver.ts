8bca3c053242d5520926ad3b1730b430cc6a7b14
Kaveh <74491598+KavehHashemi@users.noreply.github.com>
2022-11-23 12:01:22 +0330

    removed github page

--- functions/runserver.ts
+++
@@ -1,5 +0,0 @@
-const runServer = () => {
-  var oShell = new ActiveXObject("Shell.Application");
-  var commandtoRun = ".\\server\\nats-server";
-  oShell.ShellExecute(commandtoRun, "", "", "open", "1");
-};
